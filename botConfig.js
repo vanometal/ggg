c78c0a27bfac4343f3f5a9411065be46d2931cc2
vanometal <rhevanosaputra15@gmail.com>
2022-06-12 13:02:57 +0800

    ggg

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "ODY3NTg1MzM4MzMxNDk2NDc4.GgbxAM.RftKomHIe9rgPIZ7RzRWVKl2z5trzdwfn2zrXU",
+    prefix: "V",
+    admins: [
+        "660097014268428318"
+],
+    debug: true,
+    countChannel: "985393884345557062"
+};
